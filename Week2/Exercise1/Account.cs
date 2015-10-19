cdb2154223022fc43ed6aa94223903ecdf73197a
Daniel2015 <danielkopev@gmail.com>
2015-10-19 05:45:48 +0300

    refactored

---
+++ Week2/Exercise1/Account.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week2
+{
+    public class Account
+    {
+        private int anualInterestRate;
+        private int balance;
+        private int dateCreated;
+        private int id;
+
+        public Account(decimal initialBalance)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Account()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int AnualInterestRate
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        private decimal Balance
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int DateCreated
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void Deposit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Withdraw()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Week2/Exercise1/Time.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week2
+{
+    public class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Time(string format)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Hour
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
